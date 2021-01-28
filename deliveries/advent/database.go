692cbea254971d74360e326e300ea34072c8f6d4
KingDiegoA <d.sanmartinn10@gmail.com>
2021-01-28 12:01:28 -0300

    add database

---
+++ deliveries/advent/database.go
@@ -0,0 +1,29 @@
+package logs
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	_ "github.com/lib/pq"
+)
+
+// Connection - holds an open connection to database.
+type Connection struct {
+	DB *sql.DB
+}
+
+// NewConnection - returns new Connection based on arguments provided.
+func NewConnection(user, password, dbname string, logger log.Logger) Connection {
+	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+
+	connection, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		logger = level.NewFilter(logger, level.AllowInfo())
+		level.Error(logger).Log("db connection error", err)
+		panic(err)
+	}
+
+	return Connection{DB: connection}
+}
